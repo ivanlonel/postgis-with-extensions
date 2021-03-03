406787ffa1496cd2f95ec744abf12084ec23b031
ivanlonel <ivanlonel91@gmail.com>
2021-03-03 00:14:14 -0300

    Probably doesn't make a difference, but...

--- conf.sh
+++ conf.sh
@@ -15,10 +15,10 @@ listen_addresses = '*'
 shared_preload_libraries = '$(echo "$PREVIOUS_PRELOAD_LIBRARIES,$NEW_PRELOAD_LIBRARIES" | sed 's/^,//')'
 
 # pg_cron
-cron.database_name = '${POSTGRES_DB:-postgres}'
+cron.database_name = '${POSTGRES_DB:-${POSTGRES_USER:-postgres}}'
 
 ## pg_partman
-#pg_partman_bgw.dbname = '${POSTGRES_DB:-postgres}'
+#pg_partman_bgw.dbname = '${POSTGRES_DB:-${POSTGRES_USER:-postgres}}'
 
 # pglogical and wal2json
 wal_level = 'logical'
