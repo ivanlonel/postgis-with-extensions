be70ebd0d290f73303296f6dbbdc46f8bd7ab3b0
ivanlonel <ivanlonel91@gmail.com>
2021-03-03 18:40:12 -0300

    Use secrets in docker-compose

---
+++ pgadmin_entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+# Currently, the original entrypoint script only reads credentials from environment.
+# This alternative entrypoint allows reading them from files 
+
+file_env() {
+   local var="$1"
+   local fileVar="${var}_FILE"
+   local def="${2:-}"
+
+   if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+      echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+      exit 1
+   fi
+   local val="$def"
+   if [ "${!var:-}" ]; then
+      val="${!var}"
+   elif [ "${!fileVar:-}" ]; then
+      val="$(< "${!fileVar}")"
+   fi
+   export "$var"="$val"
+   unset "$fileVar"
+}
+
+file_env "PGADMIN_DEFAULT_EMAIL"
+file_env "PGADMIN_DEFAULT_PASSWORD"
+
+source /entrypoint.sh
