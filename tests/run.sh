fdcac73c63a90c65f8cb1b839c5d6d9cc4992540
ivanlonel <ivanlonel91@gmail.com>
2021-03-08 15:31:26 -0300

    Add tests/run.sh

---
+++ tests/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#./wait-for-it.sh sut:5432 --timeout=600 --strict -- echo "postgres is up"
+while ! timeout 1 psql postgres -h postgres -p 5432 -U postgres -c 'SELECT 1'; do
+    sleep 1
+done
+
+psql postgres -h postgres -p 5432 -U postgres -a -f /tests/create_extensions.sql
