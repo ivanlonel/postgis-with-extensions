421e202c3e9c917eae3f610481382c14a577fb24
ivanlonel <ivanlonel91@gmail.com>
2021-03-08 17:23:26 -0300

    Forgot to provide password for psql in test script

--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+PG_URI="postgresql://postgres:postgres@postgres:5433/postgres"
+
 #./wait-for-it.sh sut:5432 --timeout=600 --strict -- echo "postgres is up"
-while ! psql postgres -h postgres -p 5432 -U postgres -c 'SELECT 1'; do
+while ! psql $PG_URI -c 'SELECT 1'; do
     sleep 1
 done
 
-psql postgres -h postgres -p 5432 -U postgres -a -f /tests/create_extensions.sql
+psql $PG_URI -a -f /tests/create_extensions.sql
