e28c298992cd9bd53fcf800b46a99032c9b3c1e6
ivanlonel <ivanlonel91@gmail.com>
2021-03-09 00:29:47 -0300

    Actually, just drop the environment variable

--- tests/wait-for-postgres.sh
+++ tests/wait-for-postgres.sh
@@ -6,7 +6,7 @@ set -eu
 uri="$2"
 cmd="$@"
   
-until PGPASSWORD=${POSTGRES_PASSWORD:-} psql "$uri" -c '\q'; do
+until psql "$uri" -c '\q'; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
 done
