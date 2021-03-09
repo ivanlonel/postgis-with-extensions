a12526423c6fe0a131c77e386e3180d975aaa2d0
ivanlonel <ivanlonel91@gmail.com>
2021-03-09 00:59:31 -0300

    pg_cron already uses database postgres by default

--- conf.sh
+++ conf.sh
@@ -15,7 +15,8 @@ listen_addresses = '*'
 shared_preload_libraries = '$(echo "$PREVIOUS_PRELOAD_LIBRARIES,$NEW_PRELOAD_LIBRARIES" | sed 's/^,//')'
 
 # pg_cron
-cron.database_name = '${POSTGRES_DB:-${POSTGRES_USER:-postgres}}'
+# pg_cron already uses database postgres by default
+#cron.database_name = '${POSTGRES_DB:-${POSTGRES_USER:-postgres}}'
 
 ## pg_partman
 #pg_partman_bgw.dbname = '${POSTGRES_DB:-${POSTGRES_USER:-postgres}}'
