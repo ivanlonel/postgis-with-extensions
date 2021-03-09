35adc88d0f35522d1cbbc6bf680a5bb1dcf4708b
ivanlonel <ivanlonel91@gmail.com>
2021-03-08 23:41:12 -0300

    Use the hole postgres URI in wait-for-postgres.sh

--- tests/wait-for-postgres.sh
+++ tests/wait-for-postgres.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-# Copied from https://docs.docker.com/compose/startup-order/
+# Adapted from https://docs.docker.com/compose/startup-order/
 
 set -eu
   
-host="$1"
-shift
+uri="$2"
 cmd="$@"
   
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql "$uri" -c '\q'; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
 done
