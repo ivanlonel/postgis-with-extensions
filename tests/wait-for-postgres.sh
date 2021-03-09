605b026cd32ff4989ac80d38222e988ee94b2c87
ivanlonel <ivanlonel91@gmail.com>
2021-03-09 01:52:59 -0300

    Sleep 30 seconds during initial server restarts

--- tests/wait-for-postgres.sh
+++ tests/wait-for-postgres.sh
@@ -5,7 +5,10 @@ set -eu
   
 uri="$2"
 cmd="$@"
-  
+
+>&2 echo "Sleeping 30 seconds to skip initial server restarts"
+sleep 30
+
 until psql "$uri" -c '\q'; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
