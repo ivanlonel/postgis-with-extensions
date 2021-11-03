67988083784f55319c46b2285f19d33ce4e04ab7
ivanlonel <ivanlonel91@gmail.com>
2021-11-03 03:18:35 -0300

    Create more tests

--- tests/create_extensions.sql
+++ tests/create_extensions.sql
@@ -40,155 +40,453 @@ CREATE EXTENSION IF NOT EXISTS powa;
 
 -- https://github.com/pramsey/pgsql-ogr-fdw
 CREATE EXTENSION ogr_fdw;
+CREATE SERVER wraparound
+	FOREIGN DATA WRAPPER ogr_fdw
+	OPTIONS (datasource 'Pg:dbname=postgres user=postgres', format 'PostgreSQL');
+
 
 -- https://github.com/EnterpriseDB/mysql_fdw
 CREATE EXTENSION mysql_fdw;
+CREATE SERVER mysql_server
+	FOREIGN DATA WRAPPER mysql_fdw
+	OPTIONS (host '127.0.0.1', port '3306');
+
 
 -- https://github.com/laurenz/oracle_fdw
 CREATE EXTENSION oracle_fdw;
+CREATE SERVER oradb
+	FOREIGN DATA WRAPPER oracle_fdw
+	OPTIONS (dbserver '//dbserver.mydomain.com:1521/ORADB');
+
 
 -- https://github.com/pgspider/sqlite_fdw
 CREATE EXTENSION sqlite_fdw;
+CREATE SERVER sqlite_server
+	FOREIGN DATA WRAPPER sqlite_fdw
+	OPTIONS (database '/tmp/test.db');
+CREATE FOREIGN TABLE t1(a integer, b text)
+	SERVER sqlite_server
+	OPTIONS (table 't1_sqlite');
+
 
 -- https://github.com/tds-fdw/tds_fdw
 CREATE EXTENSION tds_fdw;
-
+CREATE SERVER mssql_svr
+	FOREIGN DATA WRAPPER tds_fdw
+	OPTIONS (servername '127.0.0.1', port '1433', database 'tds_fdw_test', tds_version '7.1');
 
 -- https://github.com/df7cb/pgsql-asn1oid
 CREATE EXTENSION asn1oid;
 SELECT '1.3.6.1.4.1'::asn1oid;
 
+
 -- https://github.com/xocolatl/extra_window_functions
 CREATE EXTENSION extra_window_functions;
 
+CREATE TABLE things (
+    part integer NOT NULL,
+    ord integer NOT NULL,
+    val integer
+);
+
+COPY things FROM stdin;
+1	1	64664
+1	2	8779
+1	3	14005
+1	4	57699
+1	5	98842
+1	6	88563
+1	7	70453
+1	8	82824
+1	9	62453
+2	1	\N
+2	2	51714
+2	3	17096
+2	4	41605
+2	5	15366
+2	6	87359
+2	7	98990
+2	8	34982
+2	9	3343
+3	1	21903
+3	2	24605
+3	3	6242
+3	4	24947
+3	5	79535
+3	6	66903
+3	7	42269
+3	8	31143
+3	9	\N
+4	1	\N
+4	2	49723
+4	3	23958
+4	4	80796
+4	5	\N
+4	6	41066
+4	7	72991
+4	8	33734
+4	9	\N
+5	1	\N
+5	2	\N
+5	3	\N
+5	4	\N
+5	5	\N
+5	6	\N
+5	7	\N
+5	8	\N
+5	9	\N
+\.
+
+/* FLIP_FLOP */
+SELECT part, ord, val,
+	flip_flop(val % 2 = 0) OVER w AS flip_flop_1,
+	flip_flop(val % 2 = 0, val % 2 = 1) OVER w AS flip_flop_2
+FROM things
+WINDOW w AS (PARTITION BY part ORDER BY ord ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING)
+ORDER BY part, ord;
+
+/* LAG */
+SELECT part, ord, val,
+	lag(val) OVER w AS lag,
+	lag_ignore_nulls(val) OVER w AS lag_in,
+	lag_ignore_nulls(val, 2) OVER w AS lag_in_off,
+	lag_ignore_nulls(val, 2, -9999999) OVER w AS lag_in_off_d
+FROM things
+WINDOW w AS (PARTITION BY part ORDER BY ord ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING)
+ORDER BY part, ord;
+
+/* LEAD */
+SELECT part, ord, val,
+	lead(val) OVER w AS lead,
+	lead_ignore_nulls(val) OVER w AS lead_in,
+	lead_ignore_nulls(val, 2) OVER w AS lead_in_off,
+	lead_ignore_nulls(val, 2, 9999999) OVER w AS lead_in_off_d
+FROM things
+WINDOW w AS (PARTITION BY part ORDER BY ord ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING)
+ORDER BY part, ord;
+
+/* FIRST_VALUE */
+SELECT part, ord, val,
+	first_value(val) OVER w AS fv,
+	first_value_ignore_nulls(val) OVER w AS fv_in,
+	first_value_ignore_nulls(val, 9999999) OVER w AS fv_in_d
+FROM things
+WINDOW w AS (PARTITION BY part ORDER BY ord ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING)
+ORDER BY part, ord;
+
+/* LAST_VALUE */
+SELECT part, ord, val,
+	last_value(val) OVER w AS lv,
+	last_value_ignore_nulls(val) OVER w AS lv_in,
+	last_value_ignore_nulls(val, -9999999) OVER w AS lv_in_d
+FROM things
+WINDOW w AS (PARTITION BY part ORDER BY ord ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING)
+ORDER BY part, ord;
+
+/* NTH_VALUE */
+SELECT part, ord, val,
+	nth_value(val, 3) OVER w AS nth,
+	nth_value_ignore_nulls(val, 3) OVER w AS nth_in
+FROM things
+WINDOW w AS (PARTITION BY part ORDER BY ord ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING)
+ORDER BY part, ord;
+
+SELECT part, ord, val,
+	nth_value(val, 3) OVER w AS nth,
+	nth_value_from_last(val, 3) OVER w AS nth_fl
+FROM things
+WINDOW w AS (PARTITION BY part ORDER BY ord ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING)
+ORDER BY part, ord;
+
+SELECT part, ord, val,
+	nth_value_from_last(val, 3) OVER w AS nth_fl,
+	nth_value_from_last_ignore_nulls(val, 3) OVER w AS nth_fl_in
+FROM things
+WINDOW w AS (PARTITION BY part ORDER BY ord ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING)
+ORDER BY part, ord;
+
+DROP TABLE things;
+
+
 -- https://github.com/wulczer/first_last_agg
 CREATE EXTENSION first_last_agg;
+SELECT last(x order by y) FROM (VALUES (1, 3), (2, 1), (3, 2)) AS v(x, y);
+SELECT first(x order by y) FROM (VALUES (1, 3), (2, 1), (3, 2)) AS v(x, y);
+
 
 -- https://github.com/citusdata/postgresql-hll
 CREATE EXTENSION hll;
 SELECT hll_empty();
 
+
 -- https://github.com/dverite/icu_ext
 CREATE EXTENSION icu_ext;
 SELECT * FROM icu_locales_list() where name like 'pt%';
 
+
 -- https://github.com/RhodiumToad/ip4r
 CREATE EXTENSION ip4r;
-select ipaddress '255.255.255.255' / 31;
-select ipaddress 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff' / 127;
+SELECT ipaddress '255.255.255.255' / 31;
+SELECT ipaddress 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff' / 127;
+
 
 -- https://github.com/postgrespro/jsquery
 CREATE EXTENSION jsquery;
+SELECT
+	'{"x": true}' @@ 'x IS boolean'::jsquery,
+	'{"x": 0.1}' @@ 'x IS numeric'::jsquery,
+	'{"a": {"b": 1}}' @@ 'a IS object'::jsquery,
+	'{"a": ["xxx"]}' @@ 'a IS array AND a.#: IS string'::jsquery,
+	'["xxx"]' @@ '$ IS array'::jsquery,
+	'{"points": [{"x": 1, "y": 2}, {"x": 3.9, "y": 0.5}]}' @@ 'points.#:(x IS numeric AND y IS numeric)'::jsquery;
+
 
 -- https://github.com/df7cb/postgresql-numeral
 CREATE EXTENSION numeral;
 SELECT 'thirty'::numeral + 'twelve'::numeral as sum;
 
+
 -- https://github.com/orafce/orafce
 CREATE EXTENSION orafce;
+SELECT oracle.add_months(oracle.date'2021-05-31 10:12:12', 1);
 
 -- https://github.com/xocolatl/periods
 CREATE EXTENSION periods;
 SELECT * FROM periods.periods;
 
+
 -- https://github.com/enova/pg_fact_loader
 CREATE EXTENSION pg_fact_loader;
 
+
 -- https://github.com/pgaudit/pgaudit
 CREATE EXTENSION pgaudit;
+
 
 -- https://github.com/klando/pgfincore
 CREATE EXTENSION pgfincore;
 SELECT * FROM pgsysconf_pretty();
 
+
 -- https://github.com/enova/pgl_ddl_deploy
 CREATE EXTENSION pgl_ddl_deploy;
+
 
 -- https://github.com/2ndQuadrant/pglogical
 CREATE EXTENSION pglogical;
 
+
 -- https://github.com/enova/pglogical_ticker
 CREATE EXTENSION pglogical_ticker;
 
+
 -- https://github.com/ohmu/pgmemcache
 CREATE EXTENSION pgmemcache;
+
 
 -- https://github.com/dvarrazzo/pgmp
 CREATE EXTENSION pgmp;
 SELECT 10.1::numeric::mpq;
 SELECT 9223372036854775807::mpz;
 
+
 -- https://github.com/petere/pgpcre
 CREATE EXTENSION pgpcre;
 SELECT 'foo' ~ pcre 'fo+';
 SELECT pcre 'fo+' ~ 'foo';
+
 
 -- https://github.com/pgq/pgq
 CREATE EXTENSION pgq;
 SELECT pgq.create_queue('testqueue1');
 SELECT pgq.drop_queue('testqueue1');
 
+
 -- https://github.com/pgq/pgq-node
 CREATE EXTENSION pgq_node;
+
 
 -- https://github.com/pgRouting/pgrouting
 CREATE EXTENSION pgrouting CASCADE;
 
+
 -- https://github.com/theory/pgtap
 CREATE EXTENSION pgtap;
+SELECT * FROM no_plan();
+SELECT ok(TRUE);
+SELECT * FROM finish();
+
 
 -- https://github.com/EnterpriseDB/pldebugger
 CREATE EXTENSION pldbgapi;
 
+
 -- https://github.com/pllua/pllua
 CREATE EXTENSION pllua;
+CREATE FUNCTION hello(person text) RETURNS text AS $$
+	return "Hello, " .. person .. ", from Lua!"
+$$ LANGUAGE pllua;
+SELECT hello('Fred');
+
 
 -- https://github.com/okbob/plpgsql_check
 CREATE EXTENSION plpgsql_check;
 
+SELECT p.proname, tgrelid::regclass, cf.*
+FROM pg_proc p
+    JOIN pg_trigger t ON t.tgfoid = p.oid 
+    JOIN pg_language l ON p.prolang = l.oid
+    JOIN pg_namespace n ON p.pronamespace = n.oid,
+    LATERAL plpgsql_check_function(p.oid, t.tgrelid) cf
+WHERE n.nspname = 'public' and l.lanname = 'plpgsql';
+
+
 -- https://github.com/plproxy/plproxy
 CREATE EXTENSION plproxy;
 
+
 -- https://github.com/petere/plsh
 CREATE EXTENSION plsh;
+CREATE FUNCTION concat_plsh(text, text) RETURNS text AS '
+#!/bin/sh
+echo "$1$2"
+' LANGUAGE plsh;
+SELECT concat_plsh('It ', 'works!');
+
 
 -- https://github.com/pgpointcloud/pointcloud
 CREATE EXTENSION pointcloud;
 CREATE EXTENSION pointcloud_postgis;
 
+
 -- https://github.com/dimitri/prefix
 CREATE EXTENSION prefix;
+SELECT '123'::prefix_range @> '123456';
+
 
 -- https://github.com/schmiddy/pg_prioritize
 CREATE EXTENSION prioritize;
+SELECT get_backend_priority(pg_backend_pid());
+
 
 -- https://github.com/begriffs/pg_rational
 CREATE EXTENSION pg_rational;
 SELECT 0.263157894737::float::rational;
 
+
 -- https://github.com/reorg/pg_repack
 CREATE EXTENSION pg_repack;
+
 
 -- https://github.com/postgrespro/rum
 CREATE EXTENSION rum;
 
+CREATE TABLE test_rum(t text, a tsvector);
+
+CREATE TRIGGER tsvectorupdate
+BEFORE UPDATE OR INSERT ON test_rum
+FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger('a', 'pg_catalog.english', 't');
+
+INSERT INTO test_rum(t) VALUES ('The situation is most beautiful');
+INSERT INTO test_rum(t) VALUES ('It is a beautiful');
+INSERT INTO test_rum(t) VALUES ('It looks like a beautiful place');
+
+CREATE INDEX rumidx ON test_rum USING rum (a rum_tsvector_ops);
+
+SELECT t, a <=> to_tsquery('english', 'beautiful | place') AS rank
+    FROM test_rum
+    WHERE a @@ to_tsquery('english', 'beautiful | place')
+    ORDER BY a <=> to_tsquery('english', 'beautiful | place');
+
+DROP TABLE test_rum;
+
+
 -- https://github.com/eulerto/pg_similarity
 CREATE EXTENSION pg_similarity;
+
+CREATE TABLE foo (a text);
+CREATE TABLE bar (b text);
+
+INSERT INTO foo
+	VALUES('Euler'),('Oiler'),('Euler Taveira de Oliveira'),('Maria Taveira dos Santos'),('Carlos Santos Silva');
+INSERT INTO bar
+	VALUES('Euler T. de Oliveira'),('Euller'),('Oliveira, Euler Taveira'),('Sr. Oliveira');
+
+SELECT a, b, cosine(a,b), jaro(a, b), euclidean(a, b), qgram(a, b), lev(a, b) FROM foo, bar;
+
+DROP TABLE foo;
+DROP TABLE bar;
+
 
 -- https://github.com/tvondra/tdigest
 CREATE EXTENSION tdigest;
 
+CREATE TABLE t (a int, b int, c double precision);  -- table with some random source data
+
+INSERT INTO t
+	SELECT 10 * random(), 10 * random(), random()
+	FROM generate_series(1, 100000);
+
+CREATE TABLE p AS  -- table with pre-aggregated digests into table "p"
+	SELECT a, b, tdigest(c, 100) AS d FROM t GROUP BY a, b;
+
+-- summarize the data from "p" (compute the 95-th percentile)
+SELECT a, tdigest_percentile(d, 0.95) FROM p GROUP BY a ORDER BY a;
+
+DROP TABLE t;
+DROP TABLE P;
+
+
 -- https://github.com/credativ/toastinfo
 CREATE EXTENSION toastinfo;
+
+CREATE TABLE t (
+    a text,
+    b text
+);
+
+INSERT INTO t VALUES ('null', NULL);
+INSERT INTO t VALUES ('default', 'default');
+
+ALTER TABLE t ALTER COLUMN b SET STORAGE EXTERNAL;
+INSERT INTO t VALUES ('external-10', 'external'); -- short inline varlena
+INSERT INTO t VALUES ('external-200', repeat('x', 200)); -- long inline varlena, uncompressed
+INSERT INTO t VALUES ('external-10000', repeat('x', 10000)); -- toasted varlena, uncompressed
+INSERT INTO t VALUES ('external-1000000', repeat('x', 1000000)); -- toasted varlena, uncompressed
+
+ALTER TABLE t ALTER COLUMN b SET STORAGE EXTENDED;
+INSERT INTO t VALUES ('extended-10', 'extended'); -- short inline varlena
+INSERT INTO t VALUES ('extended-200', repeat('x', 200)); -- long inline varlena, uncompressed
+INSERT INTO t VALUES ('extended-10000', repeat('x', 10000)); -- long inline varlena, compressed (pglz)
+INSERT INTO t VALUES ('extended-1000000', repeat('x', 1000000)); -- toasted varlena, compressed (pglz)
+
+ALTER TABLE t ALTER COLUMN b SET COMPRESSION lz4;
+INSERT INTO t VALUES ('extended-10000', repeat('x', 10000)); -- long inline varlena, compressed (lz4)
+INSERT INTO t VALUES ('extended-1000000', repeat('x', 1000000)); -- toasted varlena, compressed (lz4)
+
+SELECT a, length(b), pg_column_size(b), pg_toastinfo(b), pg_toastpointer(b) FROM t;
+
+DROP TABLE t;
+
 
 -- https://github.com/df7cb/postgresql-unit
 CREATE EXTENSION unit;
 SELECT '9.81 N'::unit / 'kg' AS gravity;
 
+
 -- https://www.postgresql.org/docs/current/plpython.html
 CREATE EXTENSION plpython3u;
+CREATE OR REPLACE FUNCTION py_test() RETURNS void AS $$
+with plpy.subtransaction():
+	plpy.info('UPDATE tbl SET {} = {} WHERE key = {}'.format(
+		plpy.quote_ident('Test Column'),
+		plpy.quote_nullable(None),
+		plpy.quote_literal('test value')
+	))
+$$ LANGUAGE plpython3u;
+SELECT py_test();
 
 
 \c postgres
 
-DROP DATABASE TEST;
+DROP DATABASE test;
